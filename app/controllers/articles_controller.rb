85c69edcb0264d9dcdbf49bf1426cea021b3a306
hallitee <hallitee_2005@yahoo.com>
2017-02-18 03:03:30 -0800

    show, create and index controllers done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,26 @@
 class ArticlesController < ApplicationController
+include ArticlesHelper
 
 	def index
 		@articles = Article.all
+	end
+	def show
+		@articles = Article.find(params[:id])
+	end  
+	def new
+		@articles = Article.new
 	end 
+
+	def create 
+	@articles = Article.new(article_params)
+	#@articles.title = params[:article][:title]
+	#@articles.body = params[:article][:body]
+	@articles.save
+	redirect_to article_path(@articles)
+	end
+	def destroy
+	@articles = Article.find(params[:id])
+	@articles.destroy	
+	end 
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+	validates_uniqueness_of :title, :body
+	validates_presence_of :title, :body
 end
